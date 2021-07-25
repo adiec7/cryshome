eb142373c23a1b298aa7400bb3facca1f8b26830
arudovwen <successahon@gmail.com>
2021-07-25 12:33:54 +0100

    Update index.jsx

--- src/Components/SingleHouse/index.jsx
+++ src/Components/SingleHouse/index.jsx
@@ -1,38 +1,38 @@
-import * as React from 'react';
-import './style.css';
+import * as React from "react";
+import "./style.css";
 
 class SingleHouse extends React.Component {
   render() {
     return (
-      <div className='singleHouse'>
-        <a href='#' className='card'>
-          <div className='figure'>
-            <img src={this.props.data.image_url} alt='image' />
-            <div className='figCaption'>
+      <div className="singleHouse">
+        <a href="#" className="card">
+          <div className="figure">
+            <img src={this.props.data.img} alt="image" />
+            <div className="figCaption">
               <div>#{this.props.data.price}</div>
               {/* <span className="icon-eye"> 200</span>
               <span className="icon-heart"> 54</span>
               <span className="icon-bubble"> 13</span> */}
             </div>
-            <div className='figView'>
-              <span className='icon-eye' />
+            <div className="figView">
+              <span className="icon-eye" />
             </div>
-            <div className='figType'>{this.props.data.propertyType}</div>
+            <div className="figType">{this.props.data.propertyType}</div>
           </div>
           <h2>{this.props.data.title}</h2>
-          <div className='cardAddress'>
-            <span className='icon-pointer' />
+          <div className="cardAddress">
+            <span className="icon-pointer" />
             {this.props.data.address}
           </div>
-          <ul className='cardFeat'>
+          <ul className="cardFeat">
             <li>
-              <span className='fa fa-moon-o' /> {this.props.data.beds}
+              <span className="fa fa-moon-o" /> {this.props.data.beds}
             </li>
             <li>
-              <span className='icon-drop' /> {this.props.data.toilets}
+              <span className="icon-drop" /> {this.props.data.toilets}
             </li>
             <li>
-              <span className='icon-frame' /> {this.props.data.square} Sq Ft
+              <span className="icon-frame" /> {this.props.data.square} Sq Ft
             </li>
           </ul>
         </a>
